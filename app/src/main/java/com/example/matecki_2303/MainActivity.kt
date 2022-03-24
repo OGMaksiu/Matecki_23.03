61bbc056e1c4c6bc309af7a6a6aae8f7194bcfaf
OGMaksiu <mateckimaksymilian@gmail.com>
2022-03-24 12:35:12 +0100

    Commited

---
+++ app/src/main/java/com/example/matecki_2303/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.matecki_2303
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
